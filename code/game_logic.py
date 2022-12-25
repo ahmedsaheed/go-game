2524b4a4aa6327e7577f85c8ffe7cda3cb725899
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-12-25 18:06:50 +0000

    implement

--- code/game_logic.py
+++ code/game_logic.py
@@ -1,3 +1,197 @@
+from piece import Piece
+from balls import Balls
+
+
 class GameLogic:
-    print("Game Logic Object Created")
-    # TODO add code here to manage the logic of your game
+    turn = Piece.White
+    Xpos = 0
+    Ypos = 0
+    boardArray = 0
+    whiteprisoners = 0
+    blackprisoners = 0
+    whiteterritories = 0
+    blackterritories = 0
+
+    def updateparams(self, boardArray, xpos, ypos):
+        # update current variables
+        self.Xpos = xpos
+        self.Ypos = ypos
+        self.boardArray = boardArray
+
+    def checklogic(self, boardArray, xpos, ypos):
+        # update current variables
+        self.Xpos = xpos
+        self.Ypos = ypos
+        self.boardArray = boardArray
+
+    def checkvacant(self):
+        # check if the position is vacant or not
+        if self.boardArray[self.Ypos][self.Xpos].Piece == Piece.NoPiece:
+            return True
+        else:
+            return False
+
+    def changeturn(self):
+        # function to swap turns
+        print("turn changed")
+        if self.turn == Piece.Black:
+            self.turn = Piece.White
+        else:
+            self.turn = Piece.Black
+
+    def placestone(self):
+        # function to place the stone on the board
+        if self.turn == Piece.Black:
+            self.boardArray[self.Ypos][self.Xpos].Piece = Piece.Black
+        else:
+            self.boardArray[self.Ypos][self.Xpos].Piece = Piece.White
+
+        print("Liberties = " + str(self.boardArray[self.Ypos][self.Xpos].liberties) + "x pos = " + str(
+            self.boardArray[self.Ypos][self.Xpos].x) + "y pos = " + str(self.boardArray[self.Ypos][self.Xpos].y))
+
+    def updateLiberties(self):
+        # update the liberties of all the available stones
+        count = 0
+        for row in self.boardArray:
+            for cell in row:
+                count = 0
+                if cell.Piece != Piece.NoPiece:
+                    Stonecolor = cell.Piece
+
+                    if cell.getup(self.boardArray) is not None and (
+                            cell.getup(self.boardArray).Piece == Stonecolor or cell.getup(
+                        self.boardArray).Piece == Piece.NoPiece):
+                        count = count + 1
+                    if cell.getright(self.boardArray) is not None and (
+                            cell.getright(self.boardArray).Piece == Stonecolor or cell.getright(
+                        self.boardArray).Piece == Piece.NoPiece):
+                        count = count + 1
+                    if cell.getleft(self.boardArray) is not None and (
+                            cell.getleft(self.boardArray).Piece == Stonecolor or cell.getleft(
+                        self.boardArray).Piece == Piece.NoPiece):
+                        count = count + 1
+                    if cell.getdown(self.boardArray) is not None and (
+                            cell.getdown(self.boardArray).Piece == Stonecolor or cell.getdown(
+                        self.boardArray).Piece == Piece.NoPiece):
+                        count = count + 1
+                    cell.setLiberties(count)
+
+    def updatecaptures(self):
+        # update captures of entire board, i.e. remove all stone who have 0 liberties left
+        for row in self.boardArray:
+            for cell in row:
+                if cell.liberties == 0 and cell.Piece != Piece.NoPiece:
+                    if cell.Piece == Piece.Black:
+                        self.whiteprisoners = self.whiteprisoners + 1
+                        self.boardArray[cell.y][cell.x] = Balls(Piece.NoPiece, cell.x, cell.y)
+                        print("Black Ball Captured at x: " + str(cell.x) + ", y: " + str(cell.y))
+                        return "Black Ball Captured at x: " + str(cell.x) + ", y: " + str(cell.y)
+                    elif cell.Piece == Piece.White:
+                        self.blackprisoners = self.blackprisoners + 1
+                        self.boardArray[cell.y][cell.x] = Balls(Piece.NoPiece, cell.x, cell.y)
+                        print("White Ball Captured at x: " + str(cell.x) + ", y: " + str(cell.y))
+                        return "White Ball Captured at x: " + str(cell.x) + ", y: " + str(cell.y)
+
+    def updatecaptures2(self):
+        if self.boardArray[self.Ypos][self.Xpos].getup(self.boardArray) is not None and self.boardArray[self.Ypos][
+            self.Xpos].getup(self.boardArray).liberties == 0 and self.boardArray[self.Ypos][
+            self.Xpos].getup(self.boardArray).Piece != Piece.NoPiece:
+
+            return self.capturePiece(self.Xpos, self.Ypos - 1)
+        elif self.boardArray[self.Ypos][self.Xpos].getright(self.boardArray) is not None and self.boardArray[self.Ypos][
+            self.Xpos].getright(self.boardArray).liberties == 0 and self.boardArray[self.Ypos][
+            self.Xpos].getright(self.boardArray).Piece != Piece.NoPiece:
+            return self.capturePiece(self.Xpos + 1, self.Ypos)
+        elif self.boardArray[self.Ypos][self.Xpos].getleft(self.boardArray) is not None and self.boardArray[self.Ypos][
+            self.Xpos].getleft(self.boardArray).liberties == 0 and self.boardArray[self.Ypos][
+            self.Xpos].getleft(self.boardArray).Piece != Piece.NoPiece:
+            return self.capturePiece(self.Xpos - 1, self.Ypos)
+        elif self.boardArray[self.Ypos][self.Xpos].getdown(self.boardArray) is not None and self.boardArray[self.Ypos][
+            self.Xpos].getdown(self.boardArray).liberties == 0 and self.boardArray[self.Ypos][
+            self.Xpos].getdown(self.boardArray).Piece != Piece.NoPiece:
+            return self.capturePiece(self.Xpos, self.Ypos + 1)
+
+    def capturePiece(self, xpos, ypos):
+        # captures a piece at the given position
+        if self.boardArray[ypos][xpos].Piece == 1:  # if the piece is white
+            self.blackprisoners = self.blackprisoners + 1
+            self.boardArray[ypos][xpos] = Balls(Piece.NoPiece, xpos, ypos)
+            return "White Stone Captured at x: " + str(xpos) + ", y: " + str(ypos)
+
+        else:  # if the piece is black
+            self.whiteprisoners = self.whiteprisoners + 1
+            self.boardArray[ypos][xpos] = Balls(Piece.NoPiece, xpos, ypos)
+            return "Black Stone Captured at x: " + str(xpos) + ", y: " + str(ypos)
+
+    def checkforSuicide(self):
+        oppositeplayer = Piece.NoPiece
+        if self.turn == Piece.Black:
+            oppositeplayer = Piece.White
+        else:
+            oppositeplayer = Piece.Black
+        count = 0
+        # counts the neighbouring positions for opposite stones or nulls(end of board)
+        if self.boardArray[self.Ypos][self.Xpos].getup(self.boardArray) is None or self.boardArray[self.Ypos][
+            self.Xpos].getup(self.boardArray).Piece == oppositeplayer:
+            count = count + 1
+        if self.boardArray[self.Ypos][self.Xpos].getleft(self.boardArray) is None or self.boardArray[self.Ypos][
+            self.Xpos].getleft(self.boardArray).Piece == oppositeplayer:
+            count = count + 1
+        if self.boardArray[self.Ypos][self.Xpos].getright(self.boardArray) is None or self.boardArray[self.Ypos][
+            self.Xpos].getright(self.boardArray).Piece == oppositeplayer:
+            count = count + 1
+        if self.boardArray[self.Ypos][self.Xpos].getdown(self.boardArray) is None or self.boardArray[self.Ypos][
+            self.Xpos].getdown(self.boardArray).Piece == oppositeplayer:
+            count = count + 1
+
+        if count == 4:  # this means all side are of opposite color or end of board
+            # now checking if any of the neighbours have a single liberty, if they do then by placing this stone, their liberties would turn to zero so it wont be suicide
+            if self.boardArray[self.Ypos][self.Xpos].getup(self.boardArray) is not None and self.boardArray[self.Ypos][
+                self.Xpos].getup(self.boardArray).liberties == 1:
+                return False
+            if self.boardArray[self.Ypos][self.Xpos].getleft(self.boardArray) is not None and \
+                    self.boardArray[self.Ypos][
+                        self.Xpos].getleft(self.boardArray).liberties == 1:
+                return False
+            if self.boardArray[self.Ypos][self.Xpos].getright(self.boardArray) is not None and \
+                    self.boardArray[self.Ypos][
+                        self.Xpos].getright(self.boardArray).liberties == 1:
+                return False
+            if self.boardArray[self.Ypos][self.Xpos].getdown(self.boardArray) is not None and \
+                    self.boardArray[self.Ypos][
+                        self.Xpos].getdown(self.boardArray).liberties == 1:
+                return False
+            return True
+        else:
+            return False
+
+    def getBlackPrisoner(self):
+        return str(self.blackprisoners)
+
+    def getWhitePrisoner(self):
+        return str(self.whiteprisoners)
+
+    def getBlackTerritories(self):
+        return str(self.blackterritories)
+
+    def getWhiteTerritories(self):
+        return str(self.whiteterritories)
+
+    def updateTeritories(self):
+        # update the current positions occupied by each player
+        countb = 0
+        countw = 0
+        for row in self.boardArray:
+            for cell in row:
+                if cell.Piece == Piece.Black:
+                    countb = countb + 1
+                elif cell.Piece == Piece.White:
+                    countw = countw + 1
+        self.whiteterritories = countw
+        self.blackterritories = countb
+
+    def getScore(self, Piece):
+        if Piece == 2:
+            return self.blackterritories + self.blackprisoners
+        else:
+            return self.whiteterritories + self.whiteprisoners
--- code/score_board.py
+++ code/score_board.py
@@ -1,29 +1,63 @@
-from PyQt6.QtWidgets import QDockWidget, QVBoxLayout, QWidget, QLabel
-#TODO import additional Widget classes as desired
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QColor
+from PyQt6.QtWidgets import QDockWidget, QVBoxLayout, QWidget, QLabel, QDialog, QFrame
 from PyQt6.QtCore import pyqtSlot
+from piece import Piece
+
 
 class ScoreBoard(QDockWidget):
-    """# base the score_board on a QDockWidget"""
+    '''# base the score_board on a QDockWidget'''
 
     def __init__(self):
         super().__init__()
+        self.label_PrisonersBlack = None
+        self.timerLeft = None
+        self.player_turn = None
+        self.clicker = None
+        self.instructions = None
+        self.mainLayout = None
+        self.mainWidget = None
         self.initUI()
 
     def initUI(self):
         '''initiates ScoreBoard UI'''
         self.resize(200, 200)
+        self.setFixedWidth(200)
         self.center()
         self.setWindowTitle('ScoreBoard')
-        #create a widget to hold other widgets
+        # create a widget to hold other widgets
         self.mainWidget = QWidget()
         self.mainLayout = QVBoxLayout()
 
-        #create two labels which will be updated by signals
-        self.label_clickLocation = QLabel("Click Location: ")
-        self.label_timeRemaining = QLabel("Time remaining: ")
+        # create two labels which will be updated by signals
+        self.instructions = QLabel("Instructions\n 1. Click any where to place"
+                                   "\n a stone \n 2. Press P to pass a turn \n 3. Press R to reset the Game")
+
+        self.player_turn = QLabel("Current Turn: ")
+        self.clicker = QLabel("Click Location: ")
+        self.timerLeft = QLabel("Time remaining: ")
+        self.label_PrisonersBlack = QLabel("Prisoners Taken by Black: ")
+        self.label_PrisonersWhite = QLabel("Prisoners Taken by White: ")
+        self.label_TerritoriesBlack = QLabel("Territories Taken by Black: ")
+        self.label_TerritoriesWhite = QLabel("Territories Taken by White: ")
+        col = QColor(Qt.GlobalColor.white)
+        self.frm = QFrame(self)
+        self.frm.setStyleSheet("QWidget { background-color: %s }"
+                               % col.name())
+        self.frm.setGeometry(20, 20, 100, 100)
+
         self.mainWidget.setLayout(self.mainLayout)
-        self.mainLayout.addWidget(self.label_clickLocation)
-        self.mainLayout.addWidget(self.label_timeRemaining)
+        self.mainLayout.addWidget(self.instructions)
+        self.mainLayout.addWidget(self.player_turn)
+        self.mainLayout.addWidget(self.frm)
+        # self.mainLayout.addWidget(self.passbutton)
+        self.mainLayout.addWidget(self.clicker)
+        self.mainLayout.addWidget(self.timerLeft)
+        self.mainLayout.addWidget(self.label_PrisonersBlack)
+        self.mainLayout.addWidget(self.label_PrisonersWhite)
+        self.mainLayout.addWidget(self.label_TerritoriesBlack)
+        self.mainLayout.addWidget(self.label_TerritoriesWhite)
+
         self.setWidget(self.mainWidget)
         self.show()
 
@@ -33,21 +67,127 @@ class ScoreBoard(QDockWidget):
     def make_connection(self, board):
         '''this handles a signal sent from the board class'''
         # when the clickLocationSignal is emitted in board the setClickLocation slot receives it
-        # board.clickLocationSignal.connect(self.setClickLocation)
+        board.clickLocationSignal.connect(self.setClickLocation)
         # when the updateTimerSignal is emitted in the board the setTimeRemaining slot receives it
         board.updateTimerSignal.connect(self.setTimeRemaining)
+        # when the updatePrionersSignal is emitted in the board the updatePrisoners slot receives it
+        board.updatePrionersSignal.connect(self.updatePrisoners)
+        board.updateTerritoriesSignal.connect(self.updateTerritories)
+        board.showNotificationSignal.connect(self.displaynotification)
+        board.displaychangeturnSignal.connect(self.updateturn)
 
-    @pyqtSlot(str) # checks to make sure that the following slot is receiving an argument of the type 'int'
+    @pyqtSlot(str)  # checks to make sure that the following slot is receiving an argument of the type 'int'
     def setClickLocation(self, clickLoc):
         '''updates the label to show the click location'''
-        self.label_clickLocation.setText("Click Location:" + clickLoc)
-        print('slot ' + clickLoc)
+        self.clicker.setText("Click Location:\n" + clickLoc)
+        # print('slot ' + clickLoc)
 
     @pyqtSlot(int)
     def setTimeRemaining(self, timeRemainng):
         '''updates the time remaining label to show the time remaining'''
         update = "Time Remaining:" + str(timeRemainng)
-        self.label_timeRemaining.setText(update)
-        print('slot '+update)
-        # self.redraw()
+        self.timerLeft.setText(update)
 
+    # print('slot '+update)
+    # self.redraw()
+
+
+    def updateturn(self, Piece):
+        if (Piece == 1):
+            self.player_turn.setText("Current Turn: White")
+            self.frm.setStyleSheet("QWidget { background-color: %s }"
+                                   % QColor(Qt.GlobalColor.white).name())
+        elif (Piece == 2):
+            self.player_turn.setText("Current Turn: Black")
+            self.frm.setStyleSheet("QWidget { background-color: %s }"
+                                   % QColor(Qt.GlobalColor.black).name())
+
+
+    def updatePrisoners(self, n, Player):
+        if (Player == Piece.Black):
+            update = "Prisoners Taken by Black: " + n
+            self.label_PrisonersBlack.setText(update)
+
+        elif (Player == Piece.White):
+            update = "Prisoners Taken by White: " + n
+            self.label_PrisonersWhite.setText(update)
+
+
+    def updateTerritories(self, n, Player):
+        if (Player == Piece.Black):
+            update = "Territories Taken by Black: " + n
+            self.label_TerritoriesBlack.setText(update)
+
+        elif (Player == Piece.White):
+            update = "Territories Taken by White: " + n
+            self.label_TerritoriesWhite.setText(update)
+
+
+    def passevent(self):
+        print("Pass clicked")
+
+
+    def displaynotification(self, message):
+        dlg = QDialog(self)
+        dlg.setFixedWidth(300)
+
+        dlg.setWindowTitle("Notification!")
+        self.modallayout = QVBoxLayout()
+        self.modallayout.addWidget(QLabel(message))
+        dlg.setLayout(self.modallayout)
+        dlg.exec()
+
+
+# from PyQt6.QtWidgets import QDockWidget, QVBoxLayout, QWidget, QLabel
+# #TODO import additional Widget classes as desired
+# from PyQt6.QtCore import pyqtSlot
+#
+# class ScoreBoard(QDockWidget):
+#     """# base the score_board on a QDockWidget"""
+#
+#     def __init__(self):
+#         super().__init__()
+#         self.initUI()
+#
+#     def initUI(self):
+#         '''initiates ScoreBoard UI'''
+#         self.resize(200, 200)
+#         self.center()
+#         self.setWindowTitle('ScoreBoard')
+#         #create a widget to hold other widgets
+#         self.mainWidget = QWidget()
+#         self.mainLayout = QVBoxLayout()
+#
+#         #create two labels which will be updated by signals
+#         self.label_clickLocation = QLabel("Click Location: ")
+#         self.label_timeRemaining = QLabel("Time remaining: ")
+#         self.mainWidget.setLayout(self.mainLayout)
+#         self.mainLayout.addWidget(self.label_clickLocation)
+#         self.mainLayout.addWidget(self.label_timeRemaining)
+#         self.setWidget(self.mainWidget)
+#         self.show()
+#
+#     def center(self):
+#         '''centers the window on the screen, you do not need to implement this method'''
+#
+#     def make_connection(self, board):
+#         '''this handles a signal sent from the board class'''
+#         # when the clickLocationSignal is emitted in board the setClickLocation slot receives it
+#         board.clickLocationSignal.connect(self.setClickLocation)
+#         # when the updateTimerSignal is emitted in the board the setTimeRemaining slot receives it
+#         board.updateTimerSignal.connect(self.setTimeRemaining)
+#
+#     @pyqtSlot(str) # checks to make sure that the following slot is receiving an argument of the type 'int'
+#     def setClickLocation(self, clickLoc):
+#         '''updates the label to show the click location'''
+#         self.label_clickLocation.setText("Click Location:\n" + clickLoc)
+#         # print('slot ' + clickLoc)
+#
+#     @pyqtSlot(int)
+#     def setTimeRemaining(self, timeRemainng):
+#         '''updates the time remaining label to show the time remaining'''
+#         update = "Time Remaining:\n" + str(timeRemainng)
+#         self.label_timeRemaining.setText(update)
+#         # print('slot '+update)
+#         # self.redraw()
+#
